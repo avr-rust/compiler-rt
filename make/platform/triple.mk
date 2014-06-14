7b97b8468f0614072cf3299fa8c51e85f609316f
Valerii Hiora <valerii.hiora@gmail.com>
2014-06-14 17:55:05 +0300

    Turned off compilation of sync functions
    They were built by Apple engineers and use Thumb2 instructions which were unsupported for generic Android target. Those functions are conditionally compiled on macho-embedded targets but there was no any filtering for Android

--- make/platform/triple.mk
+++ make/platform/triple.mk
@@ -23,5 +23,34 @@ endif
 # Filter out stuff that gcc cannot compile (these are only needed for clang-generated code anywasys).
 CommonFunctions_gcc := $(filter-out atomic enable_execute_stack,$(CommonFunctions))
 
-FUNCTIONS.builtins := $(CommonFunctions_gcc) $(value ArchFunctions.$(Arch))
+# Filter out stuff which is not available on specific target
+# For example, sync_fetch_and_add_4 uses Thumb instructions, which are unavailable
+# when building for arm-linux-androideabi
+ifeq ($(TargetTriple),arm-linux-androideabi)
+    ArchDisabledFunctions := \
+                sync_fetch_and_add_4 \
+                sync_fetch_and_sub_4 \
+                sync_fetch_and_and_4 \
+                sync_fetch_and_or_4 \
+                sync_fetch_and_xor_4 \
+                sync_fetch_and_nand_4 \
+                sync_fetch_and_max_4 \
+                sync_fetch_and_umax_4 \
+                sync_fetch_and_min_4 \
+                sync_fetch_and_umin_4 \
+                sync_fetch_and_add_8 \
+                sync_fetch_and_sub_8 \
+                sync_fetch_and_and_8 \
+                sync_fetch_and_or_8 \
+                sync_fetch_and_xor_8 \
+                sync_fetch_and_nand_8 \
+                sync_fetch_and_max_8 \
+                sync_fetch_and_umax_8 \
+                sync_fetch_and_min_8 \
+                sync_fetch_and_umin_8
+endif
+
+ArchEnabledFunctions := $(filter-out $(ArchDisabledFunctions),$(value ArchFunctions.$(Arch)))
+
+FUNCTIONS.builtins := $(CommonFunctions_gcc) $(ArchEnabledFunctions)
 
